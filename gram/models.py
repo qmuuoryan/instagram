89834a744e8177f7b7ee045bf524360b3d740967
ryan <you@example.com>
2019-05-20 01:21:13 +0300

    modified my views and models

--- gram/models.py
+++ gram/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class NewsLetterRecipients(models.Model):
+    name = models.CharField(max_length = 30)
+    email = models.EmailField()
--- gram/views.py
+++ gram/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import NewsLetterForm
+from django.http  import HttpResponse,Http404,HttpResponseRedirect
 
 # Create your views here.
+
+
+
+# def news_today(request):
+#     if request.method == 'POST':
+#         form = NewsLetterForm(request.POST)
+#         if form.is_valid():
+#             name = form.cleaned_data['your_name']
+#             email = form.cleaned_data['email']
+#             recipient = NewsLetterRecipients(name = name,email =email)
+#             recipient.save()
+#             HttpResponseRedirect('news_today')
+#     else:
+#         form = NewsLetterForm()
+#     return render(request, 'all-news/today-news.html', {"date": date,"news":news,"letterForm":form})
