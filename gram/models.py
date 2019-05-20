17342d3dbc94caba5faf89e1077c0b6cb97706f2
ryan <you@example.com>
2019-05-20 09:31:36 +0300

    modified models

--- gram/models.py
+++ gram/models.py
@@ -2,8 +2,28 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class NewsLetterRecipients(models.Model):
-    name = models.CharField(max_length = 30)
-    email = models.EmailField()
+# class NewsLetterRecipients(models.Model):
+#     name = models.CharField(max_length = 30)
+#     email = models.EmailField()
 
 class Profile(models.Model):    
+
+
+
+
+
+
+class Image(models.Model):  
+
+
+
+
+
+
+
+
+
+
+
+
+class Profile(models.Model):  
