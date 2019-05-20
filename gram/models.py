50df6a92e3da7822db005143b702868a00270e05
ryan <you@example.com>
2019-05-20 11:08:10 +0300

    added the profile class

--- gram/models.py
+++ gram/models.py
@@ -6,9 +6,15 @@ from django.contrib.auth.models import User
 #     name = models.CharField(max_length = 30)
 #     email = models.EmailField()
 
-class Profile(models.Model):    
-
-
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE,)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    bio = models.CharField(max_length=400)
+    profile_pic = models.ImageField(upload_to='ProfilePicture/')
+￼
+￼
+￼
 
 
 
