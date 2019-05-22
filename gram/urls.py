1241e920b2ac96af120dfc4de414772347e67428
ryan <you@example.com>
2019-05-22 18:50:41 +0300

    created a urls.py file

---
+++ gram/urls.py
@@ -0,0 +1,27 @@
+from django.conf import settings
+from django.conf.urls import url
+from . import views
+from django.conf.urls.static import static
+
+urlpatterns=[
+    url(r'^$', views.index),
+    url(r'^login', views.login),
+    
+]
+# if settings.DEBUG:
+#     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
